6d03d27fa96387537560abddeb2298fc9033dfb9
leonardomeneghin <leonardozenni@gmail.com>
2022-09-11 12:48:01 -0300

    Capturando o texto de um campo na tela

--- listadetarefas/lib/pages/todo_list_page.dart
+++ listadetarefas/lib/pages/todo_list_page.dart
@@ -5,8 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+  TodoListPage({super.key});
+  //1ª Forma para recuperar as informações que o usuário digitou no campo
+  //Precisa vincular a controller no parâmetro controller: do widget que queremos o txt.
 
+  final TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     //Scaffold não pode deixar de existir.
@@ -16,25 +19,32 @@ class TodoListPage extends StatelessWidget {
         //Adicionando padding para criar afastamento nas bordas
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'Medida',
-              hintText: 'exemplo@exemplo.com',
-              border: InputBorder.none,
-              errorText: null,
-              prefixText: 'R\$ ',
-              labelStyle: TextStyle(
-                fontSize: 20,
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController, //Vinculo com o controller
+                decoration: InputDecoration(
+                  labelText: 'E-mail',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.purple,
+                ),
               ),
-            ),
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              color: Colors.purple,
-            ),
+              ElevatedButton(
+                onPressed: login,
+                child: Text('Entrar'),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
   }
 }
